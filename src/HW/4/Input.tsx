e2091f21dd9834e7a726fb47a89750f76dfc35f9
zhig1ov <zhiglov.artem@yandex.ru>
2024-11-30 16:37:50 +0300

    Add HW4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,16 @@
+import {ChangeEvent} from 'react';
+
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (currentText: string) => void // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
-export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
+export const Input = ({currentText, setCurrentText}: InputPropsType) => {
+	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		setCurrentText(event.currentTarget.value)
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+	  <input id={'hw04-input'} type="text" value={currentText} onChange={onChangeHandler} />
+	);
 };
